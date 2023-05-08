5f49757d4f95e67e5905d6710e08b5b843d06a5e
kavi <sumathi291098@gmail.com>
2023-05-08 22:06:32 +0530

    3hrs Restarurant Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+import Navbar from './components/Navbar';
+import FoodItems from './components/FoodItems';
+import Cart from './components/Cart';
+import './assets/images/styles/FoodItems.css';
 
-function App() {
+const App = () => {
+	const [show, setShow] = useState(true);
+	const [cart , setCart] = useState([]);
+	const [warning, setWarning] = useState(false);
+
+	const handleClick = (item)=>{
+		let isPresent = false;
+		cart.forEach((product)=>{
+			if (item.id === product.id)
+			isPresent = true;
+		})
+		if (isPresent){
+			setWarning(true);
+			setTimeout(()=>{
+				setWarning(false);
+			}, 2000);
+			return ;
+		}
+		setCart([...cart, item]);
+	}
+
+	const handleChange = (item, d) =>{
+		let ind = -1;
+		cart.forEach((data, index)=>{
+			if (data.id === item.id)
+				ind = index;
+		});
+		const tempArr = cart;
+		tempArr[ind].amount += d;
+		
+		if (tempArr[ind].amount === 0)
+			tempArr[ind].amount = 1;
+		setCart([...tempArr])
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	<React.Fragment>
+		<Navbar size={cart.length} setShow={setShow} />
+		{
+			show ? <FoodItems handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+		}
+		{
+			warning && <div className='warning'>Item is already added to your cart</div>
+		}
+	</React.Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,88 @@
+//import biriyaniImg from '../src/assets/images/styles/biriyani.png';
+const list = [
+    {
+      id: 1,
+      title: "Chicken Biriyani",
+      price: 120,
+      img: "http://sautefrynbake.com/wp-content/uploads/2016/10/IMG_1580-300x225.jpg",
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "Coconutty Crispy Fried Prawns",
+      price: 128,
+      img: "http://www.sautefrynbake.com/wp-content/uploads/2014/10/image-73-300x225.jpeg",
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Crab Lollipop",
+      price: 150,
+      img: "http://sautefrynbake.com/wp-content/uploads/2016/12/FullSizeRender-62-300x225.jpg",
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "Chicken Kofta Curry",
+      price: 200,
+      img: "http://www.sautefrynbake.com/wp-content/uploads/2014/06/esckc-300x225.jpg",
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "Momo's",
+      price: 109,
+      img: "http://www.sautefrynbake.com/wp-content/uploads/2014/09/image30-300x225.jpeg",
+      amount: 1,
+    },
+    {
+      id: 6,
+      title: "Channa Pulao",
+      price: 135,
+      img: "http://sautefrynbake.com/wp-content/uploads/2013/11/channa-300x225.png",
+      amount: 1,
+    },
+    {
+      id: 7,
+      title: "Black Forest Shots",
+      price: 253,
+      img: "http://sautefrynbake.com/wp-content/uploads/2016/12/FullSizeRender-9-300x225.jpg",
+      amount: 1,
+    },
+    {
+      id: 8,
+      title: "Bread Cones",
+      price: 289,
+      img: "http://sautefrynbake.com/wp-content/uploads/2015/04/image111-300x225.jpeg",
+      amount: 1,
+    },
+    {
+      id: 9,
+      title: "Carrot Kheer",
+      price: 80,
+      img: "http://sautefrynbake.com/wp-content/uploads/2016/03/IMG_5909-300x195.jpg",
+      amount: 1,
+    },
+    {
+      id: 10,
+      title: "Fish Pickle",
+      price: 86,
+      img: "http://www.sautefrynbake.com/wp-content/uploads/2013/11/fish-300x225.jpg",
+      amount: 1,
+    },
+    {
+      id: 11,
+      title: "Vadai Kachubar Chat",
+      price: 120,
+      img: "http://sautefrynbake.com/wp-content/uploads/2015/06/image1-7-300x225.jpeg",
+      amount: 1,
+    },
+    {
+      id: 12,
+      title: "Sulaimaani Tea",
+      price: 157,
+      img: "http://sautefrynbake.com/wp-content/uploads/2017/04/FullSizeRender-1-300x251.jpg",
+      amount: 1,
+    },
+  ];
+  export default list;
